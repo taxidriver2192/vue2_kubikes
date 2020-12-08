6cce9e140dcaea1f9a96b03e2c128f646b01f1b5
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2020-12-08 12:43:03 +0100

    Update
    update

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h1>test overskrift</h1>
+      <p>{{ coordinates.lat }} Latitude, {{ coordinates.lng }} Longitude</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            coordinates:{
+                lat: 0,
+                lng: 0
+            }
+        }
+    },
+    created() {
+        // get user coordinates from browser request
+        this.$getLocation({})
+            .then(coordinates => {
+                this.coordinates = coordinates;
+            });
+
+    
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueGeolocation from 'vue-browser-geolocation'
 
 Vue.config.productionTip = false
+
+Vue.use(vueGeolocation)
 
 new Vue({
   render: h => h(App),
